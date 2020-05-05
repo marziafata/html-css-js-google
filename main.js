4348380c2056d1f1a2f8458c430af3d3147f19e8
Marzia Fatarella <marziafatarella@gmail.com>
2020-05-05 18:07:34 +0200

    dropdown

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 $(document).ready(function() {
     // intercetto il click sull'icona
+    $('body > header > div.header-right > ul > li.dropdown > a').click(function(){
+        // recupero il div con classe sottomenu che è suo fratello
+        var sottomenu = $(this).next('.sottomenu');
+        // visualizzo il sottomenù
+        sottomenu.toggle();
+    })
 
-    
 
 
 });
