d5429a84445a2366c9d4556e7fc1b778ea8a1e11
Marzia Fatarella <marziafatarella@gmail.com>
2020-05-06 10:17:21 +0200

    save

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
 $(document).ready(function() {
     // intercetto il click sull'icona
-    $('body > header > div.header-right > ul > li.dropdown > a').click(function(){
+    $('#page-wrapper > header > div.header-right > ul > li.dropdown > a').click(function(){
+        // alert('ciao');
         // recupero il div con classe sottomenu che è suo fratello
         var sottomenu = $(this).next('.sottomenu');
         // visualizzo il sottomenù
         sottomenu.toggle();
     })
-
-
-
-});
+})
